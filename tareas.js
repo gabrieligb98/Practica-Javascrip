b9ba3a6e96c7d668eefa05cda0fb23e6a3b9cfec
Gabriel Bermudez <bermudezgabrielignacio@gmail.com>
2022-06-01 23:45:38 -0300

    Tareas

---
+++ tareas.js
@@ -0,0 +1,58 @@
+//                       clase 1           
+
+// let nombre = prompt("Ingrese su nombre");
+
+// alert ("Bienvenido " + nombre);
+
+// let apellido = prompt("Ingrese su apellido");
+
+// alert ("Su apellido es " + apellido);
+
+// alert ("Nombre Completo " + nombre + " " + apellido);
+
+// let edad = parseInt(prompt("ingrese su edad"));
+
+// alert ("Su edad es " + edad);
+
+// let dia = parseInt(prompt("ingrese el dia de su nacimiento"));
+// let mes = parseInt(prompt("Ingrese el mes de nacimiento"));
+// let anio= parseInt (prompt("Ingrese el año en que usted nacio"));
+// alert (`Fecha de nacimiento: ${dia} / ${mes} / ${anio}` );
+
+
+
+
+
+
+
+//                          Clase 4 Entregable 
+
+
+
+
+
+for (let i = 1; i <= 10; i++)  {
+    let nombre = prompt("Ingrese su nombre");
+    let edad = Number(prompt("Ingrese su edad"));
+
+    //alert (`Su numero es  ${i} + Nombre  ${nombre} + " paciente de edad ${edad} años`)
+    console.log (`A usted le corresponde el N° ${i} + Nombre  ${nombre} + " paciente de edad ${edad} años`)
+}
+
+
+
+
+
+//                   Practica ejemplo
+
+// function sumar(num1,num2) {
+//     num1 + num2;
+// }
+
+// let resultado = sumar(10, 20);
+
+// console.log(resultado);
+
+
+
+
