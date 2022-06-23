acdcc1061be9db399acb7ab0c0f7c8fd3915e135
Gabriel Bermudez <bermudezgabrielignacio@gmail.com>
2022-06-23 00:10:02 -0300

    agregando eventos

--- prueba.js
+++ prueba.js
@@ -1,9 +1,35 @@
 let usuario = document.getElementById("nombre_de_usuario")
-
 usuario.innerHTML = prompt("Ingrese su nombre de usuario")
 
 
-const  productosContainer = document.querySelector('#contenedor')
+let input2  = document.getElementById("mail");
+input2.onchange = () => {console.log("valor2")};
+let input1  = document.getElementById("nombre")
+input1.addEventListener('input', () => {
+console.log(input1.value)
+})
+
+
+// let miFormulario      = document.getElementById("formulario");
+// miFormulario.addEventListener("submit", validarFormulario);
+
+// function validarFormulario(e){
+//     e.preventDefault();
+//     console.log("Formulario Enviado");    
+// }
+
+
+
+const productosContainer = document.querySelector('#contenedor')
+const carrito = [];
+const carritoContenedor = document.querySelector ('#carrito_contenedor')
+const carritoContador = document.querySelector ('#contador')
+const precioTotal = document.querySelector ('#precioTotal');
+
+
+
+//        Agregar tarjetas o card a la pagina
+
 
 
 stockAutos.forEach((items) =>{
@@ -14,10 +40,10 @@ stockAutos.forEach((items) =>{
                 <img src=${items.img} alt ="civic">
                 <h3><b>Modelo: </b>${items.modelo}</h3>
                 <p>${items.des}</p>
-                <p><b>Año: </b>${items.anio}</p>
-                <p class="precioProducto">Precio: $ ${items.precio}</p>
+                <p>Año: <b>${items.anio}</b></p>
+                <p class="precioProducto">Precio: <b>$ ${items.precio}</b></p>
                 <p> Estado: <b>${items.estado}</b></p>
-                <button class="btn-add">Agregar <i class="fas fa-shopping-cart"></i></button>
+                <button onClick ="addCar(${items.id})" class="btn-add">Agregar <i class="fas fa-shopping-cart"></i></button>
             `
 
         contenedor.append(div)
@@ -25,3 +51,54 @@ stockAutos.forEach((items) =>{
 
 
 
+//                  Funciones 
+
+
+//   funcion de intinerar en mi stock de autos y agregarlos a la bolsa de compra
+
+
+const addCar = (id) => {
+    const bolsa = stockAutos.find ((producto) => producto.id === id);
+    carrito.push(bolsa)
+    renderCarrito()
+    controlCantidad()
+    totalCarrito()
+
+}
+
+const renderCarrito = () => {
+    carritoContenedor.innerHTML = '';
+
+    carrito.forEach((item) => {
+        const div = document.createElement('div');
+        div.classList.add('productoEnCarrito');
+
+        div.innerHTML =`
+                        <p>${item.modelo}</p>
+                        <p>$ ${item.precio}</p>
+                        <button class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+                        `
+        carritoContenedor.append(div) ;             
+    })
+}
+
+
+// funcion para aumentar el contador al lado del carrito
+
+
+const controlCantidad = () => {
+    carritoContador.innerText = carrito.length;
+}
+
+
+// funcion  para llegar al total de las compras en el carrito
+
+
+const totalCarrito = () => {
+    let total = 0;
+    carrito.forEach ((producto) => {
+        total += producto.precio 
+    })
+
+    precioTotal.innerText = total
+}
--- stock.js
+++ stock.js
@@ -1,10 +1,9 @@
 let stockAutos = [
-    {id: 1, modelo: "Civic", anio:1995, des:"Coupe", precio: 400000, estado: "usado" , img: './img/civic953.jpg'},
-    {id: 2, modelo: "accord", anio:2002, des:"Sedan", precio:500000, estado: "usado" , img: './img/accord9802.png'},
-    {id: 3, modelo: "NSX", anio:1990, des:"Coupe", precio:3000000 , estado: "usado" , img: './img/honda_nsx.jpg'},
-    {id: 4, modelo: "fit", anio:2016, des:"Sedan", precio:800000, estado: "usado" , img: './img/fit16.jpg'},
-    {id: 5, modelo: "fit", anio:2021, des:"Sedan", precio:2500000, estado: "nuevo" , img: './img/fit21.jpg'},
-    {id: 6, modelo: "pilot", anio:2007, des:"4x4", precio:2350000, estado: "usado" , img: './img/honda_pilot.jpg'},
-    
-
+    {id: 1, modelo: "Civic", anio:1995, des:"Coupe", precio:400000, estado: "Usado" , img: './img/civic953.jpg'},
+    {id: 2, modelo: "Accord", anio:2002, des:"Sedan", precio:500000, estado: "Usado" , img: './img/accord9802.png'},
+    {id: 3, modelo: "NSX", anio:1990, des:"Coupe", precio:3000000 , estado: "Usado" , img: './img/honda_nsx3.jpg'},
+    {id: 4, modelo: "Fit", anio:2016, des:"Sedan", precio:800000, estado: "Usado" , img: './img/fit16.jpg'},
+    {id: 5, modelo: "Fit", anio:2021, des:"Sedan", precio:2500000, estado: "Nuevo" , img: './img/fit21.jpg'},
+    {id: 6, modelo: "Pilot", anio:2007, des:"4x4", precio:2350000, estado: "Usado" , img: './img/honda_pilot.jpg'},
+    {id: 7, modelo: "Integra", anio:1992, des:"Coupe", precio:1450000, estado: "Usado" , img: './img/integra.jpg'},
 ]
