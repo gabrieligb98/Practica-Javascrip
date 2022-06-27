4c8f48f5843a52a4aaef84c87670c1b137f34f1f
Gabriel Bermudez <bermudezgabrielignacio@gmail.com>
2022-06-26 22:57:02 -0300

    terminando la primer parte de la pagina

--- prueba.js
+++ prueba.js
@@ -1,13 +1,38 @@
-let usuario = document.getElementById("nombre_de_usuario")
-usuario.innerHTML = prompt("Ingrese su nombre de usuario")
+//          mas adelante quiero agregar la forma de colocar el nombre pero sin el prompt
+
+// let usuario = document.getElementById("nombre_usuario")
+// usuario.innerHTML = prompt("Ingrese su nombre de usuario")
+
+let usuario
+const usuarioLs = localStorage.getItem('user')
+
+if (usuarioLs) {
+    usuario =  usuarioLs
+}else {
+        usuario = prompt("Ingrese su Nombre de usuario");
+        localStorage.setItem('user', usuario);
+}
+
+const title = document.querySelector("#nombre_usuario");
+title.innerHTML = `${ usuario }`;
+const cleanLs = () =>{
+    localStorage.removeItem('user');
+    window.location.reload();
+}
+document.querySelector("#recarga").addEventListener('click', cleanLs)
 
 
-let input2  = document.getElementById("mail");
-input2.onchange = () => {console.log("valor2")};
-let input1  = document.getElementById("nombre")
-input1.addEventListener('input', () => {
-console.log(input1.value)
-})
+
+
+//  quiero ver como vincular el email con la base de datos
+
+
+// let input2  = document.getElementById("mail");
+// input2.onchange = () => {console.log("valor2")};
+// let input1  = document.getElementById("nombre")
+// input1.addEventListener('input', () => {
+// console.log(input1.value)
+// })
 
 
 // let miFormulario      = document.getElementById("formulario");
@@ -20,17 +45,19 @@ console.log(input1.value)
 
 
 
-const productosContainer = document.querySelector('#contenedor')
+const productosContainer = document.querySelector('#contenedor');
 const carrito = [];
-const carritoContenedor = document.querySelector ('#carrito_contenedor')
-const carritoContador = document.querySelector ('#contador')
+const carritoContenedor = document.querySelector ('#carrito_contenedor');
+const carritoContador = document.querySelector ('#contador');
 const precioTotal = document.querySelector ('#precioTotal');
+const btnVaciar = document.querySelector ('#vaciarCarrito');
 
 
 
 //        Agregar tarjetas o card a la pagina
 
 
+//                  Funciones 
 
 stockAutos.forEach((items) =>{
     const div = document.createElement('div');
@@ -41,7 +68,7 @@ stockAutos.forEach((items) =>{
                 <h3><b>Modelo: </b>${items.modelo}</h3>
                 <p>${items.des}</p>
                 <p>Año: <b>${items.anio}</b></p>
-                <p class="precioProducto">Precio: <b>$ ${items.precio}</b></p>
+                <p class="precioProducto">Precio: $ ${items.precio} USD</p>
                 <p> Estado: <b>${items.estado}</b></p>
                 <button onClick ="addCar(${items.id})" class="btn-add">Agregar <i class="fas fa-shopping-cart"></i></button>
             `
@@ -51,7 +78,6 @@ stockAutos.forEach((items) =>{
 
 
 
-//                  Funciones 
 
 
 //   funcion de intinerar en mi stock de autos y agregarlos a la bolsa de compra
@@ -66,18 +92,19 @@ const addCar = (id) => {
 
 }
 
+
 const renderCarrito = () => {
     carritoContenedor.innerHTML = '';
 
     carrito.forEach((item) => {
         const div = document.createElement('div');
         div.classList.add('productoEnCarrito');
-
+        
         div.innerHTML =`
-                        <p>${item.modelo}</p>
-                        <p>$ ${item.precio}</p>
-                        <button class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
-                        `
+        <p>${item.modelo}</p>
+        <p>$ ${item.precio}</p>
+        <button onclick="REMOVECARRITO(${item.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
         carritoContenedor.append(div) ;             
     })
 }
@@ -95,10 +122,39 @@ const controlCantidad = () => {
 
 
 const totalCarrito = () => {
-    let total = 0;
+    let total = 0 ;
     carrito.forEach ((producto) => {
         total += producto.precio 
     })
-
+    
     precioTotal.innerText = total
 }
+
+
+// funcion para borrar del carrito
+
+
+const REMOVECARRITO = (id) => {  
+    const item = stockAutos.find((id)=>stockAutos.id === id)
+    const indice = carrito.indexOf (item)
+    carrito.splice(indice,1);
+    
+    renderCarrito()
+    controlCantidad()
+    totalCarrito()
+}
+
+
+//        borrar todo los elementos del carro  
+
+
+const vaciarCarrito = () => {
+    carrito.length = 0;
+    renderCarrito()
+    controlCantidad()
+    totalCarrito()
+}
+
+
+btnVaciar.addEventListener('click', vaciarCarrito)
+
--- stock.js
+++ stock.js
@@ -1,9 +1,23 @@
+
 let stockAutos = [
-    {id: 1, modelo: "Civic", anio:1995, des:"Coupe", precio:400000, estado: "Usado" , img: './img/civic953.jpg'},
-    {id: 2, modelo: "Accord", anio:2002, des:"Sedan", precio:500000, estado: "Usado" , img: './img/accord9802.png'},
-    {id: 3, modelo: "NSX", anio:1990, des:"Coupe", precio:3000000 , estado: "Usado" , img: './img/honda_nsx3.jpg'},
-    {id: 4, modelo: "Fit", anio:2016, des:"Sedan", precio:800000, estado: "Usado" , img: './img/fit16.jpg'},
-    {id: 5, modelo: "Fit", anio:2021, des:"Sedan", precio:2500000, estado: "Nuevo" , img: './img/fit21.jpg'},
-    {id: 6, modelo: "Pilot", anio:2007, des:"4x4", precio:2350000, estado: "Usado" , img: './img/honda_pilot.jpg'},
-    {id: 7, modelo: "Integra", anio:1992, des:"Coupe", precio:1450000, estado: "Usado" , img: './img/integra.jpg'},
-]
+    {id: 1, modelo: "Civic", anio:1995, des:"Coupe", precio:1780, estado: "Usado" , img: './img/civic953.jpg'},
+    {id: 2, modelo: "Accord", anio:2002, des:"Sedan", precio:2250, estado: "Usado" , img: './img/accord9802.png'},
+    {id: 3, modelo: "NSX", anio:1990, des:"Coupe", precio:13350 , estado: "Usado" , img: './img/honda_nsx3.jpg'},
+    {id: 4, modelo: "Fit", anio:2016, des:"Sedan", precio:3555, estado: "Usado" , img: './img/fit16.jpg'},
+    {id: 5, modelo: "Fit", anio:2021, des:"Sedan", precio:11150, estado: "Nuevo" , img: './img/fit21.jpg'},
+    {id: 6, modelo: "Pilot", anio:2007, des:"4x4", precio:10450, estado: "Usado" , img: './img/honda_pilot.jpg'},
+    {id: 7, modelo: "Integra", anio:1992, des:"Coupe", precio:6450, estado: "Usado" , img: './img/integra.jpg'},
+    {id: 8, modelo: "Prelude", anio:1993, des: "Coupe", precio:7780, estado: "Usado", img: './img/prelude.png'},
+    {id: 9, modelo: "S2000", anio:2000, des: "Coupe", precio:12450, estado: "Usado", img: './img/hs2000.jpg'},
+]  
+
+const valorDolar = 1.76
+let totalEnUsdOficial = (autos,valorActual) => {
+    //let encontrar = stockAutos.find((producto) => producto.id === id);
+    //encontrar.precio = encontrar.precio *1.30 *1.35
+
+    autos.forEach((producto) => {producto.precio = Math.round(producto.precio*valorActual)})     
+}
+totalEnUsdOficial(stockAutos,valorDolar)
+
+
